75f12f80de38fc36f90ef8752fe4c633cf2ba839
marius.popescu <marius.popescu@softvision.com>
2022-03-04 14:23:53 +0200

    added axios get post events

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import getEvent from "../utils/getEvents";
+import postEvent from "../utils/postEvents";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>APP</div>
+      <button onClick={getEvent}>GET EVENT</button>
+      <button onClick={() => postEvent({eventType: "button-press", eventName: 'POST EVENT BUTTON'})}>POST EVENT</button>
+    </>
+  )
 };
 
 export default App;
